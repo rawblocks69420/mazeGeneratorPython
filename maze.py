7c8b49d204abd1f7276c0cc270873b6cacc96f80
rawblocks69420 <127636664+rawblocks69420@users.noreply.github.com>
2024-02-16 15:36:15 -0500

    Update maze.py
    fixed whitespaces

--- maze.py
+++ maze.py
@@ -36,37 +36,30 @@ class Cursor:
     def squares_available(self, grid):
         #check surrounding 4 squares to see if any are available.
         #returns a list off all of the positions like 'down','up','left','right'
-        
         directions = []
         #right
-
         try:
             if (grid[self.x+1][self.y] == 0):
                 directions.append('right')
         except:
             nothinga = 0
-
         try:    
             if ((grid[self.x-1][self.y] == 0) and self.x >= 1):
                 directions.append('left')
         except:
             nothinga = 0
-
         try:
             if (grid[self.x][self.y+1] == 0):
                 directions.append('down')
         except:
             nothinga = 0
-
         try:
             if ((grid[self.x][self.y-1] == 0) and self.y >= 1):
                 directions.append('up')
         except:
             nothinga = 0
 
-
         return directions
-
 
     def movement(self,grid):
         #i need to convert the grid coordinates to the line pixels
@@ -75,11 +68,7 @@ class Cursor:
         x = round((self.x * GRID_CELL_SIZE))
         y = round((self.y * GRID_CELL_SIZE))
 
-
-
         movements = self.squares_available(grid)
-
-
         move = ""
 
         try:
@@ -87,40 +76,27 @@ class Cursor:
         except:
             print("exception")
 
-
-
         if (move == "right"):
             self.linedest.append(pygame.Rect(x+GRID_CELL_SIZE,y,1,GRID_CELL_SIZE))
-            self.x += 1
-
-            
+            self.x += 1       
         elif (move == "left"):
             self.linedest.append(pygame.Rect(x,y,1,GRID_CELL_SIZE))
-            self.x -= 1
-
-            
+            self.x -= 1    
         elif (move == "up"):
             self.linedest.append(pygame.Rect(x,y,GRID_CELL_SIZE,1))
-            self.y -= 1
-
-            
+            self.y -= 1   
         elif (move == "down"):
             self.linedest.append(pygame.Rect(x,y+GRID_CELL_SIZE,GRID_CELL_SIZE,1))
             self.y += 1
-
-
         else:
             self.backtrack(grid)
             #i need it to backtrack and then call movement, and then move.
 
-
-        
         self.backlist.append([self.x,self.y])
         grid[self.x][self.y] = 1
 
         
     def backtrack(self,grid):
-
         while(True):
             item = self.backlist.pop()
             self.x = item[0]
@@ -128,12 +104,10 @@ class Cursor:
 
             movements = self.squares_available(grid)
 
-
             #if there is a way, then it returns
             if(len(movements)>0):
                 return
-            
-            
+                     
     def getLinedest(self):
         return self.linedest
     
@@ -148,24 +122,15 @@ for x in range(0, 15):#SCREEN_WIDTH // GRID_CELL_SIZE, GRID_CELL_SIZE):
         row.append(0)
     grid.append(row)
   
-
-
 # Main game loop
 running = True
-
 
 #main list
 linelist = []
 
-
-
-
 curse = Cursor(3,4)
-
-
 clock = pygame.time.Clock()
 while running:
-    
     # Fill the screen with white
     screen.fill(WHITE)
     # Draw the grid
@@ -179,23 +144,17 @@ while running:
             x = round((cellcounter * GRID_CELL_SIZE)-GRID_CELL_SIZE)
             y = round((rowcounter * GRID_CELL_SIZE)-GRID_CELL_SIZE)
             
-
             #instead of drawing lines, i want to create rect objects.
-
             #draws the box
 
             #top line
             rect = pygame.Rect(x,y,GRID_CELL_SIZE,1)
-
             #bottom line
             rect2 = pygame.Rect(x,y+GRID_CELL_SIZE,GRID_CELL_SIZE,1)
-
             #left line
             rect3 = pygame.Rect(x,y,1,GRID_CELL_SIZE)
-
             #right line
             rect4 = pygame.Rect(x+GRID_CELL_SIZE,y,1,GRID_CELL_SIZE)
-
 
             if cell >= 1:
                 pygame.draw.rect(screen,(255,200,255),pygame.Rect(x,y,GRID_CELL_SIZE,GRID_CELL_SIZE))
@@ -216,11 +175,9 @@ while running:
         if (rect in curse.getLinedest()):
             continue
         pygame.draw.rect(screen,(255,0,5),rect)
-
+        
     curse.movement(grid)
             
-
-
     # Update the screen
     pygame.display.flip()
     # Check for events
